ef5b9d91fc546bc0fa5795f1faa23e7ea531648b
thomasbrasp <thomasbrasp@outlook.com>
2026-02-13 10:46:20 +0100

    .gitignore fix, hopefully last time

--- backend/Thomas.Todo/Features/Todos/Models/TodoItem.cs
+++ backend/Thomas.Todo/Features/Todos/Models/TodoItem.cs
@@ -4,6 +4,7 @@ public class TodoItem
 {
     public int Id { get; set; }
     public string? Name { get; set; }
+
     public bool IsComplete { get; set; }
 
     public TodoItem(global::Todo.Features.Todos.Entities.Todo todoItem) =>
